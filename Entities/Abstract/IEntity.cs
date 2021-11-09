29c9afd6344fadc2bc70e45dd1cc940131c9cffd
HPInc <HPInc@UGUROMENPC>
2021-11-09 12:39:18 -0600

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity is implemented by a DB Table
+    public interface IEntity
+    {
+    }
+}
